fb5d6ef246dab2bf2e5a13dc9d59c735d37ccccb
Tiago Lopes <tiagoltavares2002@gmail.com>
2026-03-01 17:26:06 -0300

    chore: add API documentation with Swagger and validation with fastify type provider zod

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,12 +1,60 @@
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUI from '@fastify/swagger-ui'
 import fastify from 'fastify'
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider
+} from 'fastify-type-provider-zod'
+import { z } from 'zod'
 import { env } from './env.js'
 
 const app = fastify({
   logger: true
 })
 
-app.get('/', () => {
-  return { hello: 'world' }
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+await app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Bootcamp Treinos API',
+      description: 'API para o bootcamp de treinos do FSC',
+      version: '1.0.0'
+    },
+    servers: [
+      {
+        description: 'Local',
+        url: `http://localhost:${env.PORT}`
+      }
+    ]
+  },
+  transform: jsonSchemaTransform
+})
+
+await app.register(fastifySwaggerUI, {
+  routePrefix: '/docs'
+})
+
+app.withTypeProvider<ZodTypeProvider>().route({
+  method: 'GET',
+  url: '/',
+  schema: {
+    description: 'Verify if API is running',
+    tags: ['health'],
+    response: {
+      200: z.object({
+        message: z.string()
+      })
+    }
+  },
+  handler: () => {
+    return {
+      message: 'API is running!'
+    }
+  }
 })
 
 try {
