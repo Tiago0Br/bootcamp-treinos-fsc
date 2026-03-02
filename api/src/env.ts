7cc890885c86018d86246c90964b37b7829796a4
Tiago Lopes <tiagoltavares2002@gmail.com>
2026-03-02 11:11:28 -0300

    chore: setting up Prisma ORM

--- api/src/env.ts
+++ api/src/env.ts
@@ -1,7 +1,8 @@
 import { z } from 'zod'
 
 const envSchema = z.object({
-  PORT: z.coerce.number().default(3000)
+  PORT: z.coerce.number().default(3000),
+  DATABASE_URL: z.string()
 })
 
 const result = envSchema.safeParse(process.env)
---
+++ api/src/lib/prisma.ts
@@ -0,0 +1,10 @@
+import { PrismaPg } from '@prisma/adapter-pg'
+import { env } from '../env.js'
+import { PrismaClient } from '../generated/prisma/client.js'
+
+const connectionString = env.DATABASE_URL
+
+const adapter = new PrismaPg({ connectionString })
+const prisma = new PrismaClient({ adapter })
+
+export { prisma }
